df846d1f3cf5e5ad036b129c235fcbc69ba8b3da
小妹 <cuiling.liu@callfanai.com>
2025-08-15 10:32:05 +0800

    feat:去掉less

--- apps/web-ele/src/views/licenseManage/components/searchForm.vue
+++ apps/web-ele/src/views/licenseManage/components/searchForm.vue
@@ -2,7 +2,7 @@
  * @Author: 小妹 cuiling.liu@callfanai.com
  * @Date: 2025-07-31 16:07:34
  * @LastEditors: 小妹 cuiling.liu@callfanai.com
- * @LastEditTime: 2025-08-12 17:03:41
+ * @LastEditTime: 2025-08-15 10:31:54
  * @FilePath: \workSpace\vben-web\apps\web-ele\src\views\licenseManage\components\searchForm.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -83,7 +83,7 @@ const [SearchForm, formApi] = useVbenForm({
   <SearchForm />
 </template>
 
-<style lang="less" scoped>
+<style scoped>
 :deep(.search-row-flex) {
   display: flex !important;
   flex-direction: row !important;
