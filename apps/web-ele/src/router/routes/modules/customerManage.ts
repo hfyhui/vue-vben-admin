52ead2b60165917cc395942b7c41f7fe51e2839b
小妹 <cuiling.liu@callfanai.com>
2026-04-21 13:43:59 +0800

    feat:修改

--- apps/web-ele/src/router/routes/modules/customerManage.ts
+++ apps/web-ele/src/router/routes/modules/customerManage.ts
@@ -1,11 +1,3 @@
-/*
- * @Author: 小妹 cuiling.liu@callfanai.com
- * @Date: 2025-08-12 15:23:19
- * @LastEditors: 小妹 cuiling.liu@callfanai.com
- * @LastEditTime: 2026-04-09 16:15:25
- * @FilePath: \workSpace\callfans-platform-admin\apps\web-ele\src\router\routes\modules\customerManage.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import type { RouteRecordRaw } from 'vue-router';
 
 import { $t } from '#/locales';
@@ -173,6 +165,24 @@ const routes: RouteRecordRaw[] = [
         meta: {
           title: $t('page.dashboard.socialMediaAccount'),
           order: 2,
+        },
+      },
+      {
+        name: 'ApplicationSystemProxyManage',
+        path: 'proxyManage',
+        component: () => import('#/views/systemManage/proxyManage/index.vue'),
+        meta: {
+          title: $t('page.dashboard.proxyManage'),
+          order: 3,
+        },
+      },
+      {
+        name: 'ApplicationSystemContainerResourceManage',
+        path: 'containerResourceManage',
+        component: () => import('#/views/systemManage/containerResourceManage/index.vue'),
+        meta: {
+          title: $t('page.dashboard.containerResourceManage'),
+          order: 4,
         },
       },
     ],
